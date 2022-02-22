8297c2706244f35b82771a410ea293d3a50496fa
sinku <sininoir@gmail.com>
2022-02-23 02:15:41 +0330

    Fix gitignore problem

--- src/main/java/CommandHandler.java
+++ src/main/java/CommandHandler.java
@@ -1,18 +1,23 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class CommandHandler {
     private static final String EMPTY_STRING = "";
 
     public static void handleCommand(String name, String data) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
         switch (name) {
             case EMPTY_STRING:
                 break;
             case "addActor":
-                // code block
+                Actor new_actor = objectMapper.readValue(data, Actor.class);
+                new_actor.printData();
                 break;
             case "addMovie":
                 // code block
                 break;
             case "addUser":
-                // code block
+                User new_user = objectMapper.readValue(data, User.class);
+                new_user.printData();
                 break;
             case "addComment":
                 // code block
