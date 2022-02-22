797649bdb5ef0c1fa462a236dd4c45b3bf3b488a
sinku <sininoir@gmail.com>
2022-02-22 22:11:52 +0330

    Add base User class

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,34 @@
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Date;
+
 public class User {
+    private final String email;
+    private final String password;
+    private final String nickname;
+    private final String name;
+    private final Date birthDate;
+
+    @JsonCreator
+    public User(
+            @JsonProperty("email") String email,
+            @JsonProperty("password") String password,
+            @JsonProperty("nickname") String nickname,
+            @JsonProperty("name") String name,
+            @JsonProperty("birthDate") Date birthDate) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public void printData() {
+        System.out.println(this.email);
+        System.out.println(this.password);
+        System.out.println(this.nickname);
+        System.out.println(this.name);
+        System.out.println(this.birthDate);
+    }
 }
