c7f982e0f1122eb5b7d37305df71f7d36f4ca274
sina-negarandeh <negarandeh.sina@gmail.com>
2022-02-23 11:26:00 +0330

    Add setters and getters

--- src/main/java/Comment.java
+++ src/main/java/Comment.java
@@ -1,2 +1,29 @@
 public class Comment {
+    private String userEmail;
+    private Integer movieId;
+    private String text;
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Integer getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Integer movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
