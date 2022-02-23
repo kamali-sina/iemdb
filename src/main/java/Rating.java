a72356614b2c76962887abea0efa858efda2f591
sina-negarandeh <negarandeh.sina@gmail.com>
2022-02-23 11:26:09 +0330

    Add setters and getters

--- src/main/java/Rating.java
+++ src/main/java/Rating.java
@@ -1,2 +1,29 @@
 public class Rating {
+    private String userEmail;
+    private Integer movieId;
+    private Integer score;
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Integer getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Integer movieId) {
+        this.movieId = movieId;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
 }
