f887281a647b33f65e4570efd282518843bd0d15
sina-negarandeh <negarandeh.sina@gmail.com>
2022-02-23 11:41:16 +0330

    Add getters and setters for movies in movie manager

--- src/main/java/MovieManager.java
+++ src/main/java/MovieManager.java
@@ -1,2 +1,13 @@
+import java.util.HashMap;
+
 public class MovieManager {
+    static final HashMap<Integer, Movie> movies = new HashMap<>();
+
+    public static Movie getMovie(Integer movieId) {
+        return MovieManager.movies.get(movieId);
+    }
+
+    public static void addMovie(Movie movie) {
+        MovieManager.movies.put(movie.getId(), movie);
+    }
 }
