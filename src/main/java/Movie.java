d095ca63d5545fa72e63c79d1258ad3f3ab70638
sina-negarandeh <negarandeh.sina@gmail.com>
2022-02-23 11:25:48 +0330

    Add setters and getters

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -1,2 +1,101 @@
 public class Movie {
+    private Integer id;
+    private String name;
+    private String summary;
+    private String releaseDate;
+    private String director;
+    private String[] writers;
+    private String[] genres;
+    private Integer[] cast;
+    private Float imdbRate;
+    private Integer duration;
+    private Integer ageLimit;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String[] getWriters() {
+        return writers;
+    }
+
+    public void setWriters(String[] writers) {
+        this.writers = writers;
+    }
+
+    public String[] getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String[] genres) {
+        this.genres = genres;
+    }
+
+    public Integer[] getCast() {
+        return cast;
+    }
+
+    public void setCast(Integer[] cast) {
+        this.cast = cast;
+    }
+
+    public Float getImdbRate() {
+        return imdbRate;
+    }
+
+    public void setImdbRate(Float imdbRate) {
+        this.imdbRate = imdbRate;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public Integer getAgeLimit() {
+        return ageLimit;
+    }
+
+    public void setAgeLimit(Integer ageLimit) {
+        this.ageLimit = ageLimit;
+    }
 }
