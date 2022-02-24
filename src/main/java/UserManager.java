c3bde02a5f87d83cc230763ac508d289158cb420
sina-negarandeh <negarandeh.sina@gmail.com>
2022-02-24 14:36:58 +0330

    Add return message for user manager methods

--- src/main/java/UserManager.java
+++ src/main/java/UserManager.java
@@ -1,12 +1,15 @@
+import exception.CommandException;
+import exception.ErrorType;
+
 import java.util.HashMap;
 
 public class UserManager {
     static final HashMap<String, User> users = new HashMap<>();
 
-    public static User getUser(String userEmail) {
+    public static User getUser(String userEmail) throws CommandException {
         User user = UserManager.users.get(userEmail);
         if (user == null) {
-//            TODO: throw
+            throw new CommandException(ErrorType.UserNotFound);
         }
         return user;
     }
@@ -15,28 +18,28 @@ public class UserManager {
         return UserManager.users.containsKey(userEmail);
     }
 
-    public static String addUser(User user) {
-        String response = "user added successfully";
+    public static String addUser(User user) throws CommandException {
         if (UserManager.doesUserExist(user.getEmail())) {
-            response = "user updated successfully";
+            throw new CommandException(ErrorType.UserAlreadyExists);
         }
+
         UserManager.users.put(user.getEmail(), user);
-         return response;
+         return  "user added successfully";
     }
 
-    public static String addToWatchList(WatchList watchList) {
+    public static String addToWatchList(WatchList watchList) throws CommandException {
         User user = getUser(watchList.getUserEmail());
         Movie movie = MovieManager.getMovie(watchList.getMovieId());
+
         user.addToWatchList(movie);
-        return "movie added to watchlist";
+        return "movie added to watchlist successfully";
     }
 
-    public static String removeFromWatchList(WatchList watchList) {
+    public static String removeFromWatchList(WatchList watchList) throws CommandException {
         User user = getUser(watchList.getUserEmail());
         Movie movie = MovieManager.getMovie(watchList.getMovieId());
+
         user.removeFromWatchList(movie);
-        return "movie removed from watchlist";
+        return "movie removed from watchlist successfully";
     }
-
-
 }
