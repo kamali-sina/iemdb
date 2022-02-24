4cf2d7c4138344f99eeab1936215706d614601df
sinku <sininoir@gmail.com>
2022-02-24 12:38:23 +0330

    Update UserManager

--- src/main/java/UserManager.java
+++ src/main/java/UserManager.java
@@ -1,2 +1,42 @@
+import java.util.HashMap;
+
 public class UserManager {
+    static final HashMap<String, User> users = new HashMap<>();
+
+    public static User getUser(String userEmail) {
+        User user = UserManager.users.get(userEmail);
+        if (user == null) {
+//            TODO: throw
+        }
+        return user;
+    }
+
+    public static boolean doesUserExist(String userEmail) {
+        return UserManager.users.containsKey(userEmail);
+    }
+
+    public static String addUser(User user) {
+        String response = "user added successfully";
+        if (UserManager.doesUserExist(user.getEmail())) {
+            response = "user updated successfully";
+        }
+        UserManager.users.put(user.getEmail(), user);
+         return response;
+    }
+
+    public static String addToWatchList(WatchList watchList) {
+        User user = getUser(watchList.getUserEmail());
+        Movie movie = MovieManager.getMovie(watchList.getMovieId());
+        user.addToWatchList(movie);
+        return "movie added to watchlist";
+    }
+
+    public static String removeFromWatchList(WatchList watchList) {
+        User user = getUser(watchList.getUserEmail());
+        Movie movie = MovieManager.getMovie(watchList.getMovieId());
+        user.removeFromWatchList(movie);
+        return "movie removed from watchlist";
+    }
+
+
 }
