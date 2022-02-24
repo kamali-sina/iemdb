d4fe1e5390074430d55e2ddc4e9f5ca3c6088257
sina-negarandeh <negarandeh.sina@gmail.com>
2022-02-24 14:34:32 +0330

    Update comment fields

--- src/main/java/Comment.java
+++ src/main/java/Comment.java
@@ -1,7 +1,21 @@
+import java.time.LocalDate;
+
 public class Comment {
+    private static Integer count = 1;
+
     private String userEmail;
     private Integer movieId;
     private String text;
+    private Integer id;
+    private LocalDate date;
+
+    public static Integer getCount() {
+        return count++;
+    }
+
+    public static void setCount(Integer count) {
+        Comment.count = count;
+    }
 
     public String getUserEmail() {
         return userEmail;
@@ -25,5 +39,29 @@ public class Comment {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public String print() {
+        String value = "";
+        value += this.userEmail;
+        value += " " + this.movieId.toString();
+        value += " " + this.text;
+        return value;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
     }
 }
