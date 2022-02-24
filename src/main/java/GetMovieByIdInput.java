3e1f06de9cfac2e6f72ff96535c2c78a64727378
sina-negarandeh <negarandeh.sina@gmail.com>
2022-02-24 22:20:40 +0330

    Add a class for getMovieById input data

---
+++ src/main/java/GetMovieByIdInput.java
@@ -0,0 +1,11 @@
+public class GetMovieByIdInput {
+    private Integer movieId;
+
+    public Integer getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(Integer movieId) {
+        this.movieId = movieId;
+    }
+}
