46b4e975729d4e32a4145e2f776590487feec377
sinku <sininoir@gmail.com>
2022-02-24 12:38:49 +0330

    Add watchlist class

---
+++ src/main/java/WatchList.java
@@ -0,0 +1,20 @@
+public class WatchList {
+    private int movieId;
+    private String userEmail;
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+}
