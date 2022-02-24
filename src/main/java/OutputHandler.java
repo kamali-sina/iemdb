0e73a421d53227300cc1d3a1c9cac1479c5c5c81
sina-negarandeh <negarandeh.sina@gmail.com>
2022-02-24 22:28:39 +0330

    Fix handleOutput to print output properly

--- src/main/java/OutputHandler.java
+++ src/main/java/OutputHandler.java
@@ -1,13 +1,13 @@
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class OutputHandler {
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-
-    public static void handleOutput(ArrayList<String> results) throws JsonProcessingException {
+    public static void handleOutput(ArrayList<String> results) throws IOException {
         Output output = new Output(results.get(0), results.get(1));
-        System.out.println(objectMapper.writeValueAsString(output));
+        OutputHandler.printOutput(output);
+    }
+
+    public static void printOutput(Output output) throws IOException {
+        System.out.println(output.getSerializedOutput());
     }
 }
