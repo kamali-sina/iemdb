8a180d1b7776ddb5c9228c0c258bb8ace63d9882
sina-negarandeh <negarandeh.sina@gmail.com>
2022-02-24 11:21:52 +0330

    Add setters and getters for movie

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -1,15 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Movie {
     private Integer id;
     private String name;
     private String summary;
     private String releaseDate;
     private String director;
-    private String[] writers;
-    private String[] genres;
-    private Integer[] cast;
+    private ArrayList<String> writers;
+    private ArrayList<String> genres;
+    private ArrayList<Integer> cast;
     private Float imdbRate;
     private Integer duration;
     private Integer ageLimit;
+    private HashMap<String, Rating> ratings = new HashMap<>();
+    private Integer ratingCount = 0;
+    private HashMap<String, ArrayList<Comment>> comments = new HashMap<>();
 
     public Integer getId() {
         return id;
@@ -51,27 +57,27 @@ public class Movie {
         this.director = director;
     }
 
-    public String[] getWriters() {
+    public ArrayList<String> getWriters() {
         return writers;
     }
 
-    public void setWriters(String[] writers) {
+    public void setWriters(ArrayList<String> writers) {
         this.writers = writers;
     }
 
-    public String[] getGenres() {
+    public ArrayList<String> getGenres() {
         return genres;
     }
 
-    public void setGenres(String[] genres) {
+    public void setGenres(ArrayList<String> genres) {
         this.genres = genres;
     }
 
-    public Integer[] getCast() {
+    public ArrayList<Integer> getCast() {
         return cast;
     }
 
-    public void setCast(Integer[] cast) {
+    public void setCast(ArrayList<Integer> cast) {
         this.cast = cast;
     }
 
@@ -97,5 +103,57 @@ public class Movie {
 
     public void setAgeLimit(Integer ageLimit) {
         this.ageLimit = ageLimit;
+    }
+
+    public HashMap<String, Rating> getRatings() {
+        return ratings;
+    }
+
+    public void setRatings(HashMap<String, Rating> ratings) {
+        this.ratings = ratings;
+    }
+
+    public Integer getRatingCount() {
+        return ratingCount;
+    }
+
+    public void setRatingCount(Integer ratingCount) {
+        this.ratingCount = ratingCount;
+    }
+
+    public HashMap<String, ArrayList<Comment>> getComments() {
+        return comments;
+    }
+
+    public void setComments(HashMap<String, ArrayList<Comment>> comments) {
+        this.comments = comments;
+    }
+
+    public void addComment(Comment comment) {
+        // java.util.UUID.randomUUID(); TODO: ADD unique ID for each comment.
+        // TODO: Add date and time for each comment.
+
+        ArrayList<Comment> userComments;
+        // Retrieve user comments
+        if (!this.comments.containsKey(comment.getUserEmail())) {
+            userComments = new ArrayList<>();
+            this.comments.put(comment.getUserEmail(), userComments);
+        } else {
+            userComments = this.comments.get(comment.getUserEmail());
+        }
+
+        // Add new comment
+        userComments.add(comment);
+
+        // Update user comments
+        this.comments.put(comment.getUserEmail(), userComments);
+    }
+
+    public void addRating(Rating rating) {
+        if (!this.ratings.containsKey(rating.getUserEmail())) {
+            this.ratingCount += 1;
+        }
+        this.ratings.put(rating.getUserEmail(), rating);
+        // TODO: Update movie's average rating
     }
 }
