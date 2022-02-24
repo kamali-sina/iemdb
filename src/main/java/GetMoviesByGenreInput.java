d133c0e1f87398f9b328500fc9a47a74706abe67
sina-negarandeh <negarandeh.sina@gmail.com>
2022-02-24 22:20:56 +0330

    Add a class for getMoviesByGenreInput input data

---
+++ src/main/java/GetMoviesByGenreInput.java
@@ -0,0 +1,11 @@
+public class GetMoviesByGenreInput {
+    private String genre;
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
