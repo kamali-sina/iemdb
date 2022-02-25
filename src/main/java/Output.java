08a2c254d76cb98c00cf636fc9ac11cfe935a8c1
sina-negarandeh <negarandeh.sina@gmail.com>
2022-02-25 21:09:03 +0330

    Clean code

--- src/main/java/Output.java
+++ src/main/java/Output.java
@@ -8,17 +8,17 @@ public class Output {
     private String success;
     private String data;
 
+    public Output(String success, String data) {
+        this.success = success;
+        this.data = data;
+    }
+
     public String getSuccess() {
         return success;
     }
 
     public void setSuccess(String success) {
         this.success = success;
-    }
-
-    public Output(String success, String data) {
-        this.success = success;
-        this.data = data;
     }
 
     public String getData() {
