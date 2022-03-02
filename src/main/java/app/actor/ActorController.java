573588e3c5fd15084840ee9fcbd0f088c1b8b064
sinku <sininoir@gmail.com>
2022-03-03 00:06:16 +0330

    Update getActor handler

--- src/main/java/app/actor/ActorController.java
+++ src/main/java/app/actor/ActorController.java
@@ -1,9 +1,24 @@
 package app.actor;
 
-import io.javalin.http.Handler;
+import io.javalin.http.*;
+import main.Actor;
+import manager.ActorManager;
 
 public class ActorController {
     public static Handler fetchActor = ctx -> {
-        ctx.result("hello actor: " + ctx.pathParam("actor_id"));
+        Integer actor_id = null;
+        try {
+            actor_id = Integer.valueOf(ctx.pathParam("actor_id"));
+        } catch (Exception exception){
+            throw new BadRequestResponse();
+        }
+        Actor actor = null;
+        try {
+            actor = ActorManager.getActor(actor_id);
+        } catch (Exception exception) {
+            throw new NotFoundResponse();
+        }
+        // TODO: add template rendering
+        ctx.result("hello actor: " + actor.getName());
     };
 }
