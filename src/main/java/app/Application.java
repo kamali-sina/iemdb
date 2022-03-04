d29e77b316c45220062bc39c855bb723ce8b1942
sina-negarandeh <negarandeh.sina@gmail.com>
2022-03-04 17:07:11 +0330

    Add remove from user watch list service

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -18,6 +18,7 @@ public class Application {
             app.get("actors/{actor_id}", ActorController.fetchActor);
             app.get("watchList/{user_id}", UserController.fetchUserWatchList);
             app.get("watchList/{user_id}/{movie_id}", UserController.handleAddMovieToWatchList);
+            app.get("removeMovieFromWatchList/{user_id}/{movie_id}", UserController.handleRemoveMovieFromWatchList);
             app.get("rateMovie/{user_id}/{movie_id}/{rate}", MovieController.handleRatingMovie);
             app.get("voteComment/{user_id}/{comment_id}/{vote}", UserController.handleVotingComment);
             app.get("movies/search/{start_year}/{end_year}", MovieController.fetchMoviesByReleaseYear);
