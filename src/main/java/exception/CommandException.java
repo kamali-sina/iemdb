27a48c7eea5deef2e203845ff8da77a27ed04564
sinku <sininoir@gmail.com>
2022-03-04 19:45:53 +0330

    Add errorType field to CommandException

--- src/main/java/exception/CommandException.java
+++ src/main/java/exception/CommandException.java
@@ -2,8 +2,14 @@ package exception;
 
 public class CommandException extends Exception {
     private static final ErrorMessages errorMessages = new ErrorMessages();
+    private ErrorType errorType;
 
     public CommandException(ErrorType errorType) {
         super(errorMessages.messages.get(errorType.name()));
+        this.errorType = errorType;
+    }
+
+    public ErrorType getErrorType() {
+        return errorType;
     }
 }
