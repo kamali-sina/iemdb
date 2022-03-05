225c7b250bb1801d5dd27430a08d40694842c48e
sina-negarandeh <negarandeh.sina@gmail.com>
2022-03-05 21:11:51 +0330

    Clean code

--- src/main/java/app/user/UserController.java
+++ src/main/java/app/user/UserController.java
@@ -4,9 +4,11 @@ import exception.CommandException;
 import exception.ErrorType;
 import io.javalin.core.validation.ValidationException;
 import io.javalin.core.validation.Validator;
-import io.javalin.http.*;
-import main.*;
-import manager.MovieManager;
+import io.javalin.http.Handler;
+import main.Movie;
+import main.User;
+import main.Vote;
+import main.WatchListItem;
 import manager.UserManager;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -16,6 +18,116 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class UserController {
+    public static Handler fetchUserWatchList = ctx -> {
+        try {
+            Validator<String> userId = ctx.pathParamAsClass("user_id", String.class);
+            User user = UserManager.getUser(userId.get());
+
+            String htmlString = UserController.getWatchListHtmlString(user);
+
+            ctx.html(htmlString);
+        } catch (Exception exception) {
+            ctx.redirect("/notFound");
+        }
+    };
+    public static Handler handleAddMovieToWatchList = ctx -> {
+        try {
+            Validator<String> userId = ctx.pathParamAsClass("user_id", String.class);
+            Validator<Integer> movieId = ctx.pathParamAsClass("movie_id", Integer.class);
+            WatchListItem watchListItem = new WatchListItem(movieId.get(), userId.get());
+
+            UserManager.addToWatchList(watchListItem);
+
+            ctx.redirect("/success");
+        } catch (CommandException commandException) {
+            ArrayList<ErrorType> forbiddenErrorTypes = new ArrayList<>();
+            forbiddenErrorTypes.add(ErrorType.AgeLimitError);
+            forbiddenErrorTypes.add(ErrorType.MovieAlreadyExists);
+            if (forbiddenErrorTypes.contains(commandException.getErrorType())) {
+                ctx.redirect("/forbidden");
+            } else {
+                ctx.redirect("/notFound");
+            }
+        } catch (ValidationException validationException) {
+            ctx.redirect("/forbidden");
+        } catch (Exception exception) {
+            ctx.redirect("/notFound");
+        }
+    };
+    public static Handler handleRemoveMovieFromWatchList = ctx -> {
+        try {
+            Validator<String> userId = ctx.pathParamAsClass("user_id", String.class);
+            Validator<Integer> movieId = ctx.pathParamAsClass("movie_id", Integer.class);
+            WatchListItem watchListItem = new WatchListItem(movieId.get(), userId.get());
+
+            UserManager.removeFromWatchList(watchListItem);
+
+            ctx.redirect("/success");
+        } catch (ValidationException validationException) {
+            ctx.redirect("/forbidden");
+        } catch (Exception exception) {
+            ctx.redirect("/notFound");
+        }
+    };
+    public static Handler handleVotingComment = ctx -> {
+        try {
+            Validator<String> userId = ctx.pathParamAsClass("user_id", String.class);
+            Validator<Integer> commentId = ctx.pathParamAsClass("comment_id", Integer.class);
+            Validator<Integer> vote = ctx.pathParamAsClass("vote", Integer.class);
+
+            vote.check(score -> -1 <= score && score <= 1, "Vote value should be between 1 and -1");
+            Vote usersVote = new Vote(userId.get(), commentId.get(), vote.get());
+            UserManager.addVote(usersVote);
+
+            ctx.redirect("/success");
+        } catch (ValidationException validationException) {
+            ctx.redirect("/forbidden");
+        } catch (Exception exception) {
+            ctx.redirect("/notFound");
+        }
+    };
+    public static Handler handleVotingCommentByPost = ctx -> {
+        try {
+            Validator<String> userId = ctx.formParamAsClass("user_id", String.class);
+            Validator<Integer> commentId = ctx.pathParamAsClass("comment_id", Integer.class);
+            Validator<Integer> vote = ctx.pathParamAsClass("vote", Integer.class);
+
+            vote.check(score -> -1 <= score && score <= 1, "Vote value should be between 1 and -1");
+            Vote usersVote = new Vote(userId.get(), commentId.get(), vote.get());
+            UserManager.addVote(usersVote);
+
+            ctx.redirect("/success");
+        } catch (ValidationException validationException) {
+            ctx.redirect("/forbidden");
+        } catch (Exception exception) {
+            ctx.redirect("/notFound");
+        }
+    };
+    public static Handler handleAddMovieToWatchListPost = ctx -> {
+        try {
+            Validator<String> userId = ctx.formParamAsClass("user_id", String.class);
+            Validator<Integer> movieId = ctx.pathParamAsClass("movie_id", Integer.class);
+            WatchListItem watchListItem = new WatchListItem(movieId.get(), userId.get());
+
+            UserManager.addToWatchList(watchListItem);
+
+            ctx.redirect("/success");
+        } catch (CommandException commandException) {
+            ArrayList<ErrorType> forbiddenErrorTypes = new ArrayList<>();
+            forbiddenErrorTypes.add(ErrorType.AgeLimitError);
+            forbiddenErrorTypes.add(ErrorType.MovieAlreadyExists);
+            if (forbiddenErrorTypes.contains(commandException.getErrorType())) {
+                ctx.redirect("/forbidden");
+            } else {
+                ctx.redirect("/notFound");
+            }
+        } catch (ValidationException validationException) {
+            ctx.redirect("/forbidden");
+        } catch (Exception exception) {
+            ctx.redirect("/notFound");
+        }
+    };
+
     public static String getWatchListHtmlString(User user) throws IOException {
 
         File input = new File("src/main/resources/templates/watchlist.html");
@@ -55,119 +167,4 @@ public class UserController {
 
         return htmlString;
     }
-
-    public static Handler fetchUserWatchList = ctx -> {
-        try {
-            Validator<String> userId = ctx.pathParamAsClass("user_id", String.class);
-            User user = UserManager.getUser(userId.get());
-
-            String htmlString = UserController.getWatchListHtmlString(user);
-
-            ctx.html(htmlString);
-        } catch (Exception exception) {
-            ctx.redirect("/notFound");
-        }
-    };
-
-    public static Handler handleAddMovieToWatchList = ctx -> {
-        try {
-            Validator<String> userId = ctx.pathParamAsClass("user_id", String.class);
-            Validator<Integer> movieId = ctx.pathParamAsClass("movie_id", Integer.class);
-            WatchListItem watchListItem = new WatchListItem(movieId.get(), userId.get());
-
-            UserManager.addToWatchList(watchListItem);
-
-            ctx.redirect("/success");
-        } catch (CommandException commandException) {
-            ArrayList<ErrorType> forbiddenErrorTypes = new ArrayList<>();
-            forbiddenErrorTypes.add(ErrorType.AgeLimitError);
-            forbiddenErrorTypes.add(ErrorType.MovieAlreadyExists);
-            if (forbiddenErrorTypes.contains(commandException.getErrorType())) {
-                ctx.redirect("/forbidden");
-            } else {
-                ctx.redirect("/notFound");
-            }
-        } catch (ValidationException validationException) {
-            ctx.redirect("/forbidden");
-        } catch (Exception exception) {
-            ctx.redirect("/notFound");
-        }
-    };
-
-    public static Handler handleRemoveMovieFromWatchList = ctx -> {
-        try {
-            Validator<String> userId = ctx.pathParamAsClass("user_id", String.class);
-            Validator<Integer> movieId = ctx.pathParamAsClass("movie_id", Integer.class);
-            WatchListItem watchListItem = new WatchListItem(movieId.get(), userId.get());
-
-            UserManager.removeFromWatchList(watchListItem);
-
-            ctx.redirect("/success");
-        } catch (ValidationException validationException){
-            ctx.redirect("/forbidden");
-        } catch (Exception exception) {
-            ctx.redirect("/notFound");
-        }
-    };
-
-    public static Handler handleVotingComment = ctx -> {
-        try {
-            Validator<String> userId = ctx.pathParamAsClass("user_id", String.class);
-            Validator<Integer> commentId = ctx.pathParamAsClass("comment_id", Integer.class);
-            Validator<Integer> vote = ctx.pathParamAsClass("vote", Integer.class);
-
-            vote.check(score -> -1 <= score && score <= 1, "Vote value should be between 1 and -1");
-            Vote usersVote = new Vote(userId.get(), commentId.get(), vote.get());
-            UserManager.addVote(usersVote);
-
-            ctx.redirect("/success");
-        } catch (ValidationException validationException){
-            ctx.redirect("/forbidden");
-        } catch (Exception exception) {
-            ctx.redirect("/notFound");
-        }
-    };
-
-    public static Handler handleVotingCommentByPost = ctx -> {
-        try {
-            Validator<String> userId = ctx.formParamAsClass("user_id", String.class);
-            Validator<Integer> commentId = ctx.pathParamAsClass("comment_id", Integer.class);
-            Validator<Integer> vote = ctx.pathParamAsClass("vote", Integer.class);
-
-            vote.check(score -> -1 <= score && score <= 1, "Vote value should be between 1 and -1");
-            Vote usersVote = new Vote(userId.get(), commentId.get(), vote.get());
-            UserManager.addVote(usersVote);
-
-            ctx.redirect("/success");
-        } catch (ValidationException validationException){
-            ctx.redirect("/forbidden");
-        } catch (Exception exception) {
-            ctx.redirect("/notFound");
-        }
-    };
-
-    public static Handler handleAddMovieToWatchListPost = ctx -> {
-        try {
-            Validator<String> userId = ctx.formParamAsClass("user_id", String.class);
-            Validator<Integer> movieId = ctx.pathParamAsClass("movie_id", Integer.class);
-            WatchListItem watchListItem = new WatchListItem(movieId.get(), userId.get());
-
-            UserManager.addToWatchList(watchListItem);
-
-            ctx.redirect("/success");
-        } catch (CommandException commandException) {
-            ArrayList<ErrorType> forbiddenErrorTypes = new ArrayList<>();
-            forbiddenErrorTypes.add(ErrorType.AgeLimitError);
-            forbiddenErrorTypes.add(ErrorType.MovieAlreadyExists);
-            if (forbiddenErrorTypes.contains(commandException.getErrorType())) {
-                ctx.redirect("/forbidden");
-            } else {
-                ctx.redirect("/notFound");
-            }
-        } catch (ValidationException validationException) {
-            ctx.redirect("/forbidden");
-        } catch (Exception exception) {
-            ctx.redirect("/notFound");
-        }
-    };
 }
