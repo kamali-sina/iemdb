a8824a84041c1b6763fb0ca872d9505c561163f6
sina-negarandeh <negarandeh.sina@gmail.com>
2022-03-09 16:05:40 +0330

    Add login page servlet

---
+++ src/main/java/com/iemdb/LoginServlet.java
@@ -0,0 +1,30 @@
+package com.iemdb;
+
+import exception.CommandException;
+import main.User;
+import manager.UserManager;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "LoginServlet", value = "/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/jsps/login.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String userEmail = request.getParameter("user_email");
+        try {
+            User user = UserManager.getUser(userEmail);
+            UserManager.logInUser(user);
+            response.sendRedirect("/");
+        } catch (CommandException commandException) {
+            request.getRequestDispatcher("/jsps/404.jsp").forward(request, response);
+        }
+    }
+}
