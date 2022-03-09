6a9265631e59d3320384102f1105489cae3a85df
sinku <sininoir@gmail.com>
2022-03-09 14:13:14 +0330

    Add base project files and Remove javalin components

--- src/main/java/IEMDB.java
+++ src/main/java/IEMDB.java
@@ -1,7 +1,3 @@
-import app.Application;
-import handler.CommandHandler;
-import handler.InputHandler;
-import handler.OutputHandler;
 import input.DataReader;
 
 import java.util.ArrayList;
@@ -12,17 +8,6 @@ public class IEMDB {
     public static void main(String[] args) {
         try {
             DataReader.readDataFromUrl(externalServicesUrl);
-            Application.initialize();
-
-            while (true) {
-                ArrayList<String> commandArgs = InputHandler.handleInput();
-                String commandName = commandArgs.get(0);
-                String commandData = commandArgs.get(1);
-
-                ArrayList<String> results =
-                        CommandHandler.handleCommand(commandName, commandData);
-                OutputHandler.handleOutput(results);
-            }
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
         }
