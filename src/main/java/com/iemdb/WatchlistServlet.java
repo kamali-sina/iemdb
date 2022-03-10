9a6995ba0f34e6b1bd95bb473e79d2ef366d5a1f
sinku <sininoir@gmail.com>
2022-03-10 22:26:18 +0330

    Add watchlist servlet

---
+++ src/main/java/com/iemdb/WatchlistServlet.java
@@ -0,0 +1,36 @@
+package com.iemdb;
+
+import exception.CommandException;
+import main.Movie;
+import main.User;
+import manager.ErrorManager;
+import manager.MovieManager;
+import manager.UserManager;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "WatchlistServlet", value = "/watchlist")
+public class WatchlistServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/jsps/watchlist.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String movie_id_string = request.getParameter("movie_id");
+        try {
+            Integer movie_id = Integer.valueOf(movie_id_string);
+            Movie movie = MovieManager.getMovie(movie_id);
+            UserManager.getLoggedInUser().removeFromWatchList(movie);
+            response.sendRedirect("/watchlist");
+        } catch (CommandException commandException) {
+            ErrorManager.error(request, response, commandException.getMessage());
+        } catch (Exception exception) {
+            ErrorManager.error(request, response, "Invalid value for move_id");
+        }
+    }
+}
