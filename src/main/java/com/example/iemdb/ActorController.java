3d8a6812f5cb9fc5842c895edd6df5a345ad1309
sina-negarandeh <negarandeh.sina@gmail.com>
2022-04-21 19:05:37 +0430

    Create actor controller

---
+++ src/main/java/com/example/iemdb/ActorController.java
@@ -0,0 +1,23 @@
+package com.example.iemdb;
+
+import exception.CommandException;
+import main.Actor;
+import manager.ActorManager;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/actors")
+public class ActorController {
+
+    @RequestMapping("/")
+    public Collection<Actor> getActors() {
+        return ActorManager.actors.values();
+    }
+
+    @GetMapping("/{id}")
+    public Actor getActor(@PathVariable Integer id) throws CommandException {
+        return ActorManager.getActor(id);
+    }
+}
