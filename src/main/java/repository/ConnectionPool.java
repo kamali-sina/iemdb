08ea1e761dec20bb8291429a729d08bd1bbe23f1
sinku <sininoir@gmail.com>
2022-05-03 15:46:18 +0430

    Add ConnectionPool class

---
+++ src/main/java/repository/ConnectionPool.java
@@ -0,0 +1,48 @@
+package repository;
+
+
+import org.apache.commons.dbcp.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectionPool {
+    private static BasicDataSource ds = new BasicDataSource();
+    private final static String dbURL = "jdbc:mysql://localhost:3306/IemdbDatabase";
+    private final static String dbUserName = "root";
+    private final static String dbPassword = "password";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        ds.setUsername(dbUserName);
+        ds.setPassword(dbPassword);
+        ds.setUrl(dbURL);
+        ds.setMinIdle(5);
+        ds.setMaxIdle(10);
+        ds.setMaxOpenPreparedStatements(100);
+        setEncoding();
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+
+    public static void setEncoding(){
+        try {
+            Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            statement.execute("ALTER DATABASE IemdbDatabase CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
+            connection.close();
+            statement.close();
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
