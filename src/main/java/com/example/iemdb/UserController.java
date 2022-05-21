2f70784da7bd5d1894e887dc523c4cb92eb0f8f4
sina-negarandeh <negarandeh.sina@gmail.com>
2022-05-21 15:25:16 +0430

    Add JWT token

--- src/main/java/com/example/iemdb/UserController.java
+++ src/main/java/com/example/iemdb/UserController.java
@@ -2,6 +2,8 @@ package com.example.iemdb;
 
 import exception.CommandException;
 import exception.ErrorType;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import main.Movie;
 import main.User;
 import manager.MovieManager;
@@ -11,9 +13,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import output.Output;
 
+import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
 import java.util.Map;
 
 @RestController
@@ -27,12 +35,26 @@ public class UserController {
         String password = User.getHash(body.get("password"));
         try {
             User user = UserManager.getUser(email, password);
-            UserManager.logInUser(user);
+
+            String SECRET_KEY = "iemdb1401";
+
+            byte[] keyBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY+SECRET_KEY+SECRET_KEY+SECRET_KEY+SECRET_KEY);
+            Key key = new SecretKeySpec(keyBytes, SignatureAlgorithm.HS256.getJcaName());
+
+            String jwtToken = Jwts.builder()
+                    .setIssuer("iemdb")
+                    .setExpiration(Date.from(Instant.now().plus(1, ChronoUnit.DAYS)))
+                    .setIssuedAt(Date.from(Instant.now()))
+                    .claim("email", user.getEmail())
+                    .signWith(key)
+                    .compact();
+
+            UserManager.logInUser(user); // TODO: Delete
+            return new Output(HttpStatus.OK.value(), jwtToken);
         } catch (Exception e) {
             response.setStatus(HttpStatus.BAD_REQUEST.value());
-            return new Output(HttpStatus.BAD_REQUEST.value(), "username and password combination is wrong.");
+            return new Output(HttpStatus.BAD_REQUEST.value(), e.getMessage());
         }
-        return new Output(HttpStatus.OK.value(), "user logged in successfully");
     }
 
     @PostMapping("/logout")
