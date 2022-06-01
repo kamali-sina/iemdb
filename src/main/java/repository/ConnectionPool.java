e598266465fa6dbe8a9ed67d8e5a5ffb4ec143d7
sinku <sininoir@gmail.com>
2022-06-01 12:26:17 +0430

    Change localhost address to connect to local machines network

--- src/main/java/repository/ConnectionPool.java
+++ src/main/java/repository/ConnectionPool.java
@@ -9,7 +9,7 @@ import java.sql.Statement;
 
 public class ConnectionPool {
     private static BasicDataSource ds = new BasicDataSource();
-    private final static String dbURL = "jdbc:mysql://localhost:3306/iemdb";
+    private final static String dbURL = "jdbc:mysql://host.docker.internal:3306/iemdb";
     private final static String dbUserName = "root";
     private final static String dbPassword = "sina1234";
 
