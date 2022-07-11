4176a66451dd1a76200af14f92d475bf3cc3fe91
sinku <sininoir@gmail.com>
2022-07-11 19:56:54 +0430

    Fix bug

--- src/main/java/repository/ConnectionPool.java
+++ src/main/java/repository/ConnectionPool.java
@@ -5,22 +5,27 @@ import org.apache.commons.dbcp.BasicDataSource;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class ConnectionPool {
     private static BasicDataSource ds = new BasicDataSource();
-    private final static String dbURL = "jdbc:mysql://iemdb-mysql:3306/iemdb";
+    private final static String dbURL = "jdbc:mysql://iemdb-mysql.sina-sina-ns:3306/";
     private final static String dbUserName = "root";
     private final static String dbPassword = "sina1234";
+    private final static String dbName = "iemdb";
 
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
+            Connection conn = DriverManager.getConnection(dbURL + "?user=" + System.getenv("DB_USERNAME" + "&" + "password=" + System.getenv("DB_PASSWORD")));
+            Statement stmt = conn.createStatement();
+            stmt.executeUpdate("CREATE DATABASE IF NOT EXISTS " + dbName);
+        } catch (ClassNotFoundException | SQLException e) {
             System.out.println(e.getMessage());
         }
-        ds.setUsername(dbUserName);
-        ds.setPassword(dbPassword);
+        ds.setUsername(System.getenv("DB_USERNAME"));
+        ds.setPassword(System.getenv("DB_PASSWORD"));
         ds.setUrl(dbURL);
         ds.setMinIdle(5);
         ds.setMaxIdle(10);
